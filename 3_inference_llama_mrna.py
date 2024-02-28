0f6f352c9b91fef7bdae65eb15883162a07ba492
Ruijiang Li <ruijiang.li@sanofi.com>
2024-02-29 07:08:41 +0800

    improve inference code

--- 3_inference_llama_mrna.py
+++ 3_inference_llama_mrna.py
@@ -4,7 +4,7 @@
 This script is adapted from TinyLlama:
 https://github.com/jzhang38/TinyLlama/blob/main/pretrain/tinyllama.py
 """
-
+import tqdm
 import math
 import os
 import sys
@@ -12,6 +12,7 @@ import time
 from functools import partial
 from pathlib import Path
 from typing import Tuple, Union
+from typing import Any, Optional, Tuple
 
 import lightning as L
 import torch
@@ -27,17 +28,10 @@ from torchmetrics.aggregation import RunningMean
 wd = Path(__file__).parent.parent.resolve()
 sys.path.append(str(wd))
 
+from lit_gpt.model import GPT, Block, CausalSelfAttention, Config, LLaMAMLP
 from lit_gpt.utils import CycleIterator, chunked_cross_entropy, num_parameters
 
-import shutil
-from mamba_ssm.models.config_mamba import MambaConfig
-from mamba_ssm.models.mixer_seq_simple import *
-from mamba_ssm.models.mixer_seq_simple import _init_weights
-from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
 
-from mamba_ssm.modules.mamba_simple import Mamba, Block
-from mamba_ssm.utils.generation import GenerationMixin
-from mamba_ssm.utils.hf import load_config_hf, load_state_dict_hf
 
 #%%
 ######### DATASET SETUP, please test the loader before proceed
@@ -80,7 +74,9 @@ from mrna_utils import *
 mrna_tok = MRNATOK()
 
 # System settings
-name = "mamba"
+model_name = "tiny-llama-1.1b"
+name = "lit-tiny-llama-1.1b"
+
 out_dir = Path(os.getenv("LIGHTNING_ARTIFACTS_DIR", "out")) / name
 logger_name = "tensorboard"
 devices = torch.cuda.device_count() or 1
@@ -89,15 +85,16 @@ devices = torch.cuda.device_count() or 1
 ############
 #### modify me please
 # global_batch_size = 512
-global_batch_size = 12
-micro_batch_size = 12 # modify me please
+
+micro_batch_size = 12*4 # modify me please
+global_batch_size = micro_batch_size*devices
+
+rope_condense_ratio = 2
 ctx_size = 8192
-
-# d_model = 2560
-# n_layer = 64
-d_model = 512
-n_layer = 12
-
+# n_embd = 2048
+# intermediate_size = 5632
+n_embd = 1024 # dont change me
+intermediate_size = 5632//2
 
 log_step_interval = 100
 eval_iters = 1000
@@ -107,7 +104,7 @@ max_tokens = ctx_size*10e6*2
 
 ############
 
-learning_rate = 1e-4
+learning_rate = 4e-4
 warmup_steps = 2000
 
 weight_decay = 1e-1
@@ -120,8 +117,13 @@ min_lr = 4e-5
 # in_data_path = "/home/lithtp/sanofi/mrna/mrna_downstream/mds/va"
 # out_data_path = "/home/lithtp/sanofi/mrna/mrna_downstream/mds_emb_mamba/va"
 
-in_data_path = "/home/lithtp/sanofi/mrna/mrna_downstream/mds/tr"
-out_data_path = "/home/lithtp/sanofi/mrna/mrna_downstream/mds_emb_mamba/tr"
+# in_data_path = "/home/lithtp/sanofi/mrna/mrna_downstream/mds/va"
+# out_data_path = "/home/lithtp/sanofi/mrna/mrna_downstream/mds_emb_llama/va"
+
+in_data_path = "/home/lithtp/sanofi/mrna/mrna_downstream/mds/va"
+out_data_path = f"/home/lithtp/sanofi/mrna/mrna_downstream/mds_emb_llama_rc_{rope_condense_ratio}/va"
+os.makedirs(f"/home/lithtp/sanofi/mrna/mrna_downstream/mds_emb_llama_rc_{rope_condense_ratio}", exist_ok=True)
+
 
 
 batch_size = global_batch_size // devices
@@ -132,6 +134,32 @@ log_iter_interval = log_step_interval * gradient_accumulation_iters
 
 
 hparams = {k: v for k, v in locals().items() if isinstance(v, (int, float, str)) and not k.startswith("_")}
+
+
+class MyGPT(GPT):
+    def forward(self, idx: torch.Tensor, input_pos: Optional[torch.Tensor] = None) -> torch.Tensor:
+        T = idx.size(1)
+        if self.max_seq_length < T:
+            raise ValueError(f"Cannot forward sequence of length {T}, max seq length is only {self.max_seq_length}.")
+
+        if input_pos is not None:  # use the kv cache
+            cos = self.cos.index_select(0, input_pos)
+            sin = self.sin.index_select(0, input_pos)
+            if self.mask_cache is None:
+                raise TypeError("You need to call `gpt.set_kv_cache()`")
+            mask = self.mask_cache.index_select(2, input_pos)
+        else:
+            cos = self.cos[:T]
+            sin = self.sin[:T]
+            mask = None
+
+        x = self.transformer.wte(idx)  # token embeddings of shape (b, t, n_embd)
+        for block in self.transformer.h:
+            x = block(x, cos, sin, mask, input_pos)
+        x = self.transformer.ln_f(x)
+        return x  # (b, t, vocab_size)
+    
+
 
 
 def setup(resume: Union[bool, Path] = False):
@@ -147,27 +175,6 @@ def setup(resume: Union[bool, Path] = False):
 
     main(fabric, resume)
 
-class MyMamba(MambaLMHeadModel):
-    
-    # def __init__(
-    #     self,
-    #     config: MambaConfig,
-    #     initializer_cfg=None,
-    #     device=None,
-    #     dtype=None,
-    # ) -> None:
-    #     print('#$$$$$$$$$$$$$$$$$')
-    #     super().__init__(config, initializer_cfg, device, dtype)
-
-    def forward(self, input_ids, position_ids=None, inference_params=None, num_last_tokens=0):
-        """
-        "position_ids" is just to be compatible with Transformer generation. We don't use it.
-        num_last_tokens: if > 0, only return the logits for the last n tokens
-        """
-        hidden_states = self.backbone.forward(input_ids, inference_params=inference_params)
-        if num_last_tokens > 0:
-            hidden_states = hidden_states[:, -num_last_tokens:]
-        return hidden_states
 
 
 def main(fabric, resume):
@@ -175,9 +182,19 @@ def main(fabric, resume):
         out_dir.mkdir(parents=True, exist_ok=True)
 
     ############## modify the config for mRNA cases
-    config = MambaConfig(d_model=d_model, n_layer=n_layer, vocab_size=len(mrna_tok.map_id_tok)+1)
+    config = Config.from_name(model_name)
+    ############## modify the config for mRNA cases
     config.block_size = ctx_size
+    config.vocab_size = len(mrna_tok.map_id_tok)
+    config.padded_vocab_size = len(mrna_tok.map_id_tok)+1
+    config.n_embd = n_embd
+    config.intermediate_size = intermediate_size
+    config.hf_config['name'] = 'mrna_tinyllama'
+    config.hf_config['org'] = 'Sanofi'
+    config.__post_init__()
+    config.rope_condense_ratio = rope_condense_ratio
     print(mrna_tok.map_id_tok)
+    #######
     #######
     print(config)
     val_dataloader = create_dataloaders(batch_size=micro_batch_size, block_size=config.block_size)
@@ -188,12 +205,12 @@ def main(fabric, resume):
     fabric.print(f"Loading model with {config.__dict__}")
     t0 = time.perf_counter()
     with fabric.init_module(empty_init=False):
-        model = MyMamba(config)
+        model = MyGPT(config)
 
     fabric.print(f"Time to instantiate model: {time.perf_counter() - t0:.02f} seconds.")
     fabric.print(f"Total parameters: {num_parameters(model):,}")
 
-    # model = torch.compile(model)
+    model = torch.compile(model)
     model = fabric.setup(model)
     optimizer = torch.optim.AdamW(
         model.parameters(), lr=learning_rate, weight_decay=weight_decay, betas=(beta1, beta2), fused=True
@@ -223,26 +240,36 @@ def main(fabric, resume):
 def compute_embeddings(fabric: L.Fabric, model: nn.Module, val_dataloader: DataLoader, max_iters: int=9999999) -> torch.Tensor:
     fabric.print("Validating ...")
     model.eval()
-    columns = {'embs':'pkl'}
+    columns = {'embs':'pkl', 'L': 'int', 'loss': 'float32'}
     shutil.rmtree(out_data_path, ignore_errors=True)
     with MDSWriter(out=out_data_path, columns=columns, compression=None, keep_local=True) as out:
-        for k, val_data in enumerate(val_dataloader):
+        for k, val_data in tqdm.tqdm(enumerate(val_dataloader)):
             if k >= max_iters:
                 break
+
             input_ids = val_data[:, 0 : model.config.block_size].contiguous().long()
+            targets = val_data[:, 1 : (model.config.block_size + 1)].contiguous().long()
+            idx_ignore_batch = input_ids<0
+            input_ids[idx_ignore_batch] = model.config.vocab_size
 
-            idx_ignore_all = input_ids<0
-            input_ids[idx_ignore_all] = model.config.vocab_size
-
-            hidden = model.forward(input_ids).cpu().numpy().astype(np.float16)
-            idx_ignore_all = idx_ignore_all.cpu().numpy()
-            
             M = input_ids.shape[0]
+            hidden = model.forward(input_ids)
+            logits = model.lm_head(hidden)
+            lst_loss = [torch.nn.functional.cross_entropy(logits[mi], targets[mi], ignore_index=-1, size_average=True, reduce=True).item() for mi in range(M)]
+            lst_L = idx_ignore_batch.long().argmax(dim=1).detach().cpu().numpy().tolist()
+            
+            hidden = hidden.cpu().numpy().astype(np.float16)
+
+
+            # print('###### ', lst_L.shape, lst_L.dtype)
+            idx_ignore_batch = idx_ignore_batch.cpu().numpy()
 
             for mi in range(M):
-                towrite = hidden[mi, ~(idx_ignore_all[mi]),:]
+                towrite = hidden[mi, ~(idx_ignore_batch[mi]),:]
                 sample = {
                     'embs': towrite,
+                    'L': lst_L[mi],
+                    'loss': lst_loss[mi]
                 }                
                 out.write(sample)
 
